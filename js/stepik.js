2bcddea92ede64862a2adf24a4e77009f29b8881
Ivan Golubev <chtoza.ivan@gmail.com>
2022-01-15 12:18:06 +0300

    Step1_new_pass

---
+++ js/stepik.js
@@ -0,0 +1,6 @@
+function myFunction() { };
+//function myFunction {document.write("Hello world!"); };
+//function Function(a) { document.write(a); };
+//function %myFunction() {document.write("Hello World!"); };
+//var myFunction = function(a) {document.write(a); } ;
+//var myFunction = (function() {document.write(a)}; );
